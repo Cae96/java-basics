d80c20b61a63351eadf991b50e33778450cdca33
Cae96 <caetano.b.alves@gmail.com>
2025-02-06 11:42:54 -0300

    ContaBanco

--- tipos variaveis/tipos-variaveis/src/App.java
+++ tipos variaveis/tipos-variaveis/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+        int numero = 5;
+
+        System.out.print(-- numero);
+        numero = numero;
     }
 }
